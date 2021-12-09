1c6aae849896eef2a5661c0b4b61877ed282e1d3
Pip Grylls <pipgryllsastro@gmail.com>
2021-12-09 09:13:35 +0000

    reformatting lesson directory structure to better match other SWC lessons

--- needed_packages.R
+++ needed_packages.R
@@ -7,7 +7,7 @@ library(hexbin) # ggplot
 library(dbplyr) # R and databases
 library(RSQLite) # R and databases
 library(tidyverse) # lesson 3 onwards
-# all of these are apart of tidyverse, but sometimes learners are unable
+# all of these are a part of tidyverse, but sometimes learners are unable
 # to install tidyverse, and need to install them individually, so here they are
 library(lubridate)
 library(readr)
