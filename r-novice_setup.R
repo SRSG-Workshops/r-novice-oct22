c8ebbed74727ce3aab60fba78e4cc4e5236b788a
Pip Grylls <pipgryllsastro@gmail.com>
2022-01-11 13:34:34 +0000

    testing syntax

--- r-novice_setup.R
+++ r-novice_setup.R
@@ -66,7 +66,7 @@ knitr::knit_hooks$set(source = function(x, options) {
                 '>',
                 unlist(strsplit(x, '\n')),
                 '{: .solution}'),
-              sep = '', collapse = '\n'),
+              sep = '', collapse = '\n')
     } else {
         hook_source(x, options)
     }
