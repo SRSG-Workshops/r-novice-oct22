80909c1fbd267261633f90fe74f140818a02dfe9
Pip Grylls <pipgryllsastro@gmail.com>
2022-01-12 13:48:47 +0000

    testing syntax

--- r-novice_setup.R
+++ r-novice_setup.R
@@ -62,13 +62,13 @@ hook_source <- knitr::knit_hooks$get("source")  # save the old hook
 knitr::knit_hooks$set(source = function(x, options) {
     if (isTruthy(options$answer)) {
         paste(
-              c('~~~', paste('> ',
+              c(paste('> ',
                       c('## Solution',
                         '',
                         unlist(strsplit(x, '\n'))
                       ),
                       sep = ''),
-                '{: .solution}', '~~~'),
+                '{: .solution}'),
               collapse = '\n')
       } else {
         hook_source(x, options)
