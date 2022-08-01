dd6a10b457b54b1963853d3962c901ea3b607529
Pip Grylls <pipgryllsastro@gmail.com>
2022-08-01 09:25:35 +0000

    updating build

--- bin/build_me.sh
+++ bin/build_me.sh
@@ -31,7 +31,7 @@ if ls _episodes_rmd/*.Rmd >/dev/null 2>&1; then
   RMD_PATH=$(find ./_episodes_rmd -name '*.Rmd')
   Rscript -e 'for (f in commandArgs(TRUE)) if (file.exists(f)) rmarkdown::render(f, knit_root_dir=getwd(), output_dir=dirname(sub("./_episodes_rmd/", "./_episodes/", f)))' ${RMD_PATH[*]}
   perl -pi -e "s/([>\s]*)(>\s)(.*?)(\{: \.[a-zA-Z]+\})/\1\2\3\n\1\4\n\1/g" ./_episodes/*.md
-  perl -0777pi -e "s/(?<!\n){: .challenge}/\n{: .challenge}/g" ./_episodes/*.md
+  perl -0777pi -e "s/(?<!\n)\{: .challenge\}/\n\{: .challenge\}/g" ./_episodes/*.md
 fi
 
 python3 bin/make_favicons.py
